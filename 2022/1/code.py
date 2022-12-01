f2bd695da1ba638793dc1674c00835d500e17118
BeardyMike <42244545+BeardyMike@users.noreply.github.com>
2022-12-01 14:34:03 +0000

    Day 1 - Part1 and Part2 complete!
    Completed.

---
+++ 2022/1/code.py
@@ -0,0 +1,10 @@
+#part 1
+with open('input.txt', 'r') as file:
+    valyoos = file.read()
+    valyoos = valyoos.split('\n\n')
+    valyoos = [q.split('\n') for q in valyoos]
+    caloreez = [sum([int(b) for b in q]) for q in valyoos]
+    print("the Single BIGGEST cache of caloreez is",max(caloreez))
+#Part 2
+    caloreez = sorted(caloreez, reverse=True)
+    print("the top three caches of caloreez added together is",caloreez[0] + caloreez[1] + caloreez[2])
