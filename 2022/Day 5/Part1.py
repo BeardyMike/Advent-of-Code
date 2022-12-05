6d5664130d973df16026bc89efd79b44f5ca4c8f
BeardyMike <42244545+BeardyMike@users.noreply.github.com>
2022-12-05 12:57:32 +0000

    Day 5 part 1 and 2 COMPLETED
    need to figure out how to process the input, hardcoding it was a cop out.

--- 2022/Day 5/Part1.py
+++ 2022/Day 5/Part1.py
@@ -1,3 +1,44 @@
 def partone():
-    
-    return
+    # I couldnt figurte out how to parse the data from the first ten lines of the file
+    # so I just hard coded the data into a dictionary, and removed the lines from the file
+    crate = {}
+    crate["1"] = ['N', 'R', 'G', 'P',]
+    crate["2"] = ['J', 'T', 'B', 'L','F', 'G', 'D', 'C',]
+    crate["3"] = ['M', 'S', 'V',]
+    crate["4"] = ['L', 'S', 'R', 'C','Z', 'P',]
+    crate["5"] = ['P', 'S', 'L', 'V','C', 'W', 'D', 'Q',]
+    crate["6"] = ['C', 'T', 'N', 'W','D', 'M', 'S',]
+    crate["7"] = ['H', 'D', 'G', 'W','P',]
+    crate["8"] = ['Z', 'L', 'P', 'H','S', 'C', 'M', 'V',]
+    crate["9"] = ['R', 'P', 'F', 'L','W', 'G', 'Z',]
+
+    # read the input file
+    with open("2022\Day 5\input.txt",'r') as f:
+        # read the input file
+        for line in f:
+            line = line.replace("move ", "")
+            line = line.replace("from ", "")
+            line = line.replace("to ", "")
+            line = line.replace(" ", ",")
+            line = line.replace("\n", "")
+            # split the line into a dictionary of the values
+            line = line.split(',')
+            cranemove = line[0]
+            cranefrom = line[1]
+            craneto = line[2]
+            #remove spaces from the new variables
+            for i in range(0, int(cranemove)):
+                crate[craneto].append(crate[cranefrom].pop())
+        print("-------Part1-------")       
+        print(crate["1"])
+        print(crate["2"])
+        print(crate["3"])
+        print(crate["4"])
+        print(crate["5"])
+        print(crate["6"])
+        print(crate["7"])
+        print(crate["8"])
+        print(crate["9"])
+        print("----End of Part1----") 
+        print()
+        print()
--- 2022/Day 5/Part2.py
+++ 2022/Day 5/Part2.py
@@ -1,3 +1,43 @@
 def parttwo():
-    
-    return
+    crate = {}
+    crate["1"] = ['N', 'R', 'G', 'P',]
+    crate["2"] = ['J', 'T', 'B', 'L','F', 'G', 'D', 'C',]
+    crate["3"] = ['M', 'S', 'V',]
+    crate["4"] = ['L', 'S', 'R', 'C','Z', 'P',]
+    crate["5"] = ['P', 'S', 'L', 'V','C', 'W', 'D', 'Q',]
+    crate["6"] = ['C', 'T', 'N', 'W','D', 'M', 'S',]
+    crate["7"] = ['H', 'D', 'G', 'W','P',]
+    crate["8"] = ['Z', 'L', 'P', 'H','S', 'C', 'M', 'V',]
+    crate["9"] = ['R', 'P', 'F', 'L','W', 'G', 'Z',]
+    crate["10"] = ['blank',]
+
+    # read the input file
+    with open("2022\Day 5\input.txt",'r') as f:
+        # read the input file
+        for line in f:
+            line = line.replace("move ", "")
+            line = line.replace("from ", "")
+            line = line.replace("to ", "")
+            line = line.replace(" ", ",")
+            line = line.replace("\n", "")
+            # split the line into a dictionary of the values
+            line = line.split(',')
+            cranemove = line[0]
+            cranefrom = line[1]
+            craneto = line[2]
+            #remove spaces from the new variables
+            for i in range(0, int(cranemove)):
+                crate["10"].append(crate[cranefrom].pop())
+            for i in range(0, int(cranemove)):   
+                crate[craneto].append(crate["10"].pop())
+        print("-------Part2-------")       
+        print(crate["1"])
+        print(crate["2"])
+        print(crate["3"])
+        print(crate["4"])
+        print(crate["5"])
+        print(crate["6"])
+        print(crate["7"])
+        print(crate["8"])
+        print(crate["9"])
+        print("----End of Part2----") 
